843edabb7854d481e8b244eb028af4ecd5836d94
felipecolor9 <felipe.m199@gmail.com>
2021-08-18 23:14:30 -0300

    Pageable GET State method added

--- src/main/java/br/lipsoft/cityapi/resource/StateResource.java
+++ src/main/java/br/lipsoft/cityapi/resource/StateResource.java
@@ -1,9 +1,23 @@
 package br.lipsoft.cityapi.resource;
 
+import br.lipsoft.cityapi.entity.State;
+import br.lipsoft.cityapi.repository.StateRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Pageable;
 
 @RestController
-@RequestMapping("/states")
+@RequestMapping("api/states")
 public class StateResource {
+
+    @Autowired
+    private StateRepository stateRepository;
+
+    @GetMapping
+    public Page<State> GetStatesByPages(Pageable page) {
+        return stateRepository.findAll(page);
+    }
 }
