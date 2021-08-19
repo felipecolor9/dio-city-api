fc201f957d7c9c678d40b2899d2a344877320fe4
felipecolor9 <felipe.m199@gmail.com>
2021-08-18 23:15:20 -0300

    Pageable GET City method add

--- src/main/java/br/lipsoft/cityapi/resource/CityResource.java
+++ src/main/java/br/lipsoft/cityapi/resource/CityResource.java
@@ -1,9 +1,23 @@
 package br.lipsoft.cityapi.resource;
 
+import br.lipsoft.cityapi.entity.City;
+import br.lipsoft.cityapi.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/cities")
+@RequestMapping("api/cities")
 public class CityResource {
+
+    @Autowired
+    private CityRepository repository;
+
+    @GetMapping
+    public Page<City> getCitiesByPage(Pageable page) {
+        return repository.findAll(page);
+    }
 }
