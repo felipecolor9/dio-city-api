e689a90734ff077d242227aeee97399fcd258ec6
felipecolor9 <felipe.m199@gmail.com>
2021-08-18 22:59:01 -0300

    State entity created

---
+++ src/main/java/br/lipsoft/cityapi/entity/State.java
@@ -0,0 +1,65 @@
+package br.lipsoft.cityapi.entity;
+
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+@NoArgsConstructor @AllArgsConstructor @Data
+@Entity(name = "State")
+@Table(name = "estado")
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
+public class State {
+
+    @Id
+    private Long id;
+
+    @Column(name = "nome")
+    private String name;
+
+    private String uf;
+
+    private Integer ibge;
+
+  /* 1st
+  @Column(name = "pais")
+  private Integer countryId;*/
+
+    // 2nd - @ManyToOne
+    @ManyToOne
+    @JoinColumn(name = "pais", referencedColumnName = "id")
+    private Country country;
+
+    @Type(type = "jsonb")
+    @Basic(fetch = FetchType.LAZY)
+    @Column(name = "ddd", columnDefinition = "jsonb")
+    private List<Integer> ddd;
+
+    /**
+     *
+     * @param id
+     * @param name
+     * @param uf
+     * @param ibge
+     * @param country
+     * @param ddd
+     */
+
+}
