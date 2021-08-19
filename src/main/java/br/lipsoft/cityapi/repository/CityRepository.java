7acc001adbf4bfb1537bf633fbea23eed790aa50
felipecolor9 <felipe.m199@gmail.com>
2021-08-18 23:13:27 -0300

    Repository created for City

---
+++ src/main/java/br/lipsoft/cityapi/repository/CityRepository.java
@@ -0,0 +1,10 @@
+package br.lipsoft.cityapi.repository;
+
+import br.lipsoft.cityapi.entity.City;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CityRepository extends JpaRepository<City, Long> {
+
+}
