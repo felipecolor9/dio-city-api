2dfa44e8a528b06aaa6d1c0c6f97ae80a8ad7df5
felipecolor9 <felipe.m199@gmail.com>
2021-08-18 23:00:05 -0300

    Get country By ID method added to Rest API

--- src/main/java/br/lipsoft/cityapi/resource/CountryResource.java
+++ src/main/java/br/lipsoft/cityapi/resource/CountryResource.java
@@ -5,7 +5,9 @@ import br.lipsoft.cityapi.repository.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,5 +28,10 @@ public class CountryResource {
     @GetMapping
     public Page<Country> getCountriesByPage(Pageable page) {
         return countryRepository.findAll(page);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getCountryById(@PathVariable Long id) {
+        return ResponseEntity.ok().body(countryRepository.getById(id));
     }
 }
